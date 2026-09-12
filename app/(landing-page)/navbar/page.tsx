a37273eb5ea015d00d8b0444767b87ecade8bcb6
Ayush Gupta <ayushguptajan22@gmail.com>
2026-09-13 00:35:18 +0530

    Update page.tsx

--- app/(landing-page)/navbar/page.tsx
+++ app/(landing-page)/navbar/page.tsx
@@ -38,7 +38,7 @@ const Navbar = () => {
             >
               <Logo className="h-10 w-10 mr-3 drop-shadow-sm" />
               <span className="font-black text-xl tracking-tighter text-slate-900">
-                Gangey Amrit<span className="text-orange-600">.</span>
+                Ganga Amrit<span className="text-orange-600">.</span>
               </span>
             </motion.div>
           </Link>
